d209eab127b095e05c42396d4ed73e9d2b79c56e
fulsel <logan.fulse10@gmail.com>
2014-10-16 08:40:06 -0400

    Got it Working
    Still need to add variation.

---
+++ SquareSpace/SquareSpace/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SquareSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
